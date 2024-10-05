35e6d207fe451e52cf972f2fc27cc24c30ab6460
duduBmoon21 <eleanortefera21@gmail.com>
2024-10-05 20:51:17 +0300

    add dummy data

---
+++ backend/data/inventory.js
@@ -0,0 +1,116 @@
+const base = [
+    {
+      item: 'Gluten-Free',
+      quantity: 120,
+      price: 0.2,
+      threshold: 15,
+    },
+    {
+      item: 'Whole Wheat',
+      quantity: 150,
+      price: 0.15,
+      threshold: 12,
+    },
+    {
+      item: 'Stuffed Crust',
+      quantity: 80,
+      price: 0.25,
+      threshold: 8,
+    },
+    {
+      item: 'Flatbread',
+      quantity: 90,
+      price: 0.12,
+      threshold: 5,
+    },
+  ];
+  
+  const sauce = [
+    {
+      item: 'Marinara',
+      quantity: 120,
+      price: 0.15,
+      threshold: 12,
+    },
+    {
+      item: 'Garlic Sauce',
+      quantity: 80,
+      price: 0.2,
+      threshold: 5,
+    },
+    {
+      item: 'Spicy Arrabbiata',
+      quantity: 100,
+      price: 0.18,
+      threshold: 10,
+    },
+    {
+      item: 'Honey BBQ',
+      quantity: 110,
+      price: 0.22,
+      threshold: 7,
+    },
+    {
+      item: 'Creamy Alfredo',
+      quantity: 90,
+      price: 0.2,
+      threshold: 6,
+    },
+  ];
+  
+  const cheese = [
+    {
+      item: 'Gorgonzola',
+      quantity: 80,
+      price: 0.2,
+      threshold: 8,
+    },
+    {
+      item: 'Goat Cheese',
+      quantity: 70,
+      price: 0.25,
+      threshold: 5,
+    },
+    {
+      item: 'Swiss Cheese',
+      quantity: 90,
+      price: 0.18,
+      threshold: 7,
+    },
+    {
+      item: 'Cream Cheese',
+      quantity: 60,
+      price: 0.3,
+      threshold: 3,
+    },
+  ];
+  
+  const veggie = [
+    {
+      item: 'Roasted Peppers',
+      quantity: 100,
+      price: 0.15,
+      threshold: 10,
+    },
+    {
+      item: 'Spinach',
+      quantity: 110,
+      price: 0.1,
+      threshold: 5,
+    },
+    {
+      item: 'Mushrooms',
+      quantity: 120,
+      price: 0.12,
+      threshold: 10,
+    },
+    {
+      item: 'Red Onions',
+      quantity: 100,
+      price: 0.1,
+      threshold: 5,
+    },
+  ];
+  
+  module.exports = { base, sauce, cheese, veggie };
+  
---
+++ backend/data/pizzas.js
@@ -0,0 +1,49 @@
+const pizzas = [
+    {
+      name: 'Vegetarian Supreme',
+      description: 'Tomato sauce, mozzarella, assorted vegetables',
+      base: 'Whole Wheat',
+      sauces: ['Marinara'],
+      cheeses: ['Mozzarella'],
+      veggies: ['Roasted Peppers', 'Spinach', 'Mushrooms'],
+      price: 7.95,
+      size: 'medium',
+      imageUrl: 'http://localhost:5173/images/vegan-pesto.jpg',
+    },
+    {
+      name: 'BBQ Chicken',
+      description: 'BBQ sauce, grilled chicken, red onions, and mozzarella',
+      base: 'Gluten-Free',
+      sauces: ['Honey BBQ'],
+      cheeses: ['Mozzarella'],
+      veggies: ['Red Onions'],
+      price: 9.95,
+      size: 'large',
+      imageUrl: 'http://localhost:5173/images/bbq-chickenpizza.jpg',
+    },
+    {
+      name: 'Pesto Delight',
+      description: 'Pesto sauce, mozzarella, fresh basil, and artichokes',
+      base: 'Flatbread',
+      sauces: ['Pesto'],
+      cheeses: ['Mozzarella'],
+      veggies: ['Artichokes', 'Basil'],
+      price: 8.95,
+      size: 'medium',
+      imageUrl: 'http://localhost:5173/images/pesto-delight.jpg',
+    },
+    {
+      name: 'Meat Lover’s',
+      description: 'Tomato sauce, mozzarella, pepperoni, sausage, and bacon',
+      base: 'Stuffed Crust',
+      sauces: ['Marinara'],
+      cheeses: ['Mozzarella'],
+      veggies: [],
+      price: 11.95,
+      size: 'large',
+      imageUrl: 'http://localhost:5173/images/images/pss.jpg',
+    },
+  ];
+  
+  module.exports = pizzas;
+  
